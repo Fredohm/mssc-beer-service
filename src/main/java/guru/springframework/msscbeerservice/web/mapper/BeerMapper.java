27651b3eef33bc40392a1d13876bcb30686b5d85
Fredohm <fredwaucquez@hotmail.fr>
2021-03-30 16:15:17 +0200

    129. Add show Inventory as Parameter. Closes #19

--- src/main/java/guru/springframework/msscbeerservice/web/mapper/BeerMapper.java
+++ src/main/java/guru/springframework/msscbeerservice/web/mapper/BeerMapper.java
@@ -4,16 +4,20 @@ import guru.springframework.msscbeerservice.domain.Beer;
 import guru.springframework.msscbeerservice.web.model.BeerDto;
 import org.mapstruct.DecoratedWith;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(uses = {DateMapper.class})
 @DecoratedWith(BeerMapperDecorator.class)
 public interface BeerMapper {
 
-//    @Mapping(target = "quantityOnHand",ignore = true)
+    @Mapping(target = "quantityOnHand",ignore = true)
     BeerDto beerToBeerDto(Beer beer);
 
+    @Mapping(target = "quantityOnHand",ignore = true)
+    BeerDto beerToBeerDtoWithInventory(Beer beer);
 
-//    @Mapping(target = "quantityToBrew", ignore = true)
-//    @Mapping(target = "minOnHand", ignore = true)
+
+    @Mapping(target = "quantityToBrew", ignore = true)
+    @Mapping(target = "minOnHand", ignore = true)
     Beer beerDtoToBeer(BeerDto beerDto);
 }
