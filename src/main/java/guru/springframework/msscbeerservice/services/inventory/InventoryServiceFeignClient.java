37a41c7ea7495facf76a4ee14108702bb0af145c
Fredohm <fredwaucquez@hotmail.fr>
2021-04-26 10:01:40 +0200

    Configure HTTP Basic for feign client. closes #40

--- src/main/java/guru/springframework/msscbeerservice/services/inventory/InventoryServiceFeignClient.java
+++ src/main/java/guru/springframework/msscbeerservice/services/inventory/InventoryServiceFeignClient.java
@@ -1,5 +1,6 @@
 package guru.springframework.msscbeerservice.services.inventory;
 
+import guru.springframework.msscbeerservice.config.FeignClientConfig;
 import guru.springframework.msscbeerservice.services.inventory.model.BeerInventoryDto;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
@@ -11,7 +12,9 @@ import java.util.List;
 import java.util.UUID;
 
 
-@FeignClient(name = "beer-inventory-service", fallback = InventoryServiceFeignClientFailover.class)
+@FeignClient(name = "beer-inventory-service",
+        fallback = InventoryServiceFeignClientFailover.class,
+        configuration = FeignClientConfig.class)
 public interface InventoryServiceFeignClient {
 
     @RequestMapping(method = RequestMethod.GET, value = BeerInventoryServiceRestTemplateImpl.INVENTORY_PATH)
