776fe08c89f60f06f280b13de2f84d6f2cc831dd
Fredohm <fredwaucquez@hotmail.fr>
2021-05-03 06:43:03 +0200

    Docker Compose and ELK Stack

--- src/main/java/guru/springframework/msscbeerservice/config/FeignClientConfig.java
+++ src/main/java/guru/springframework/msscbeerservice/config/FeignClientConfig.java
@@ -2,9 +2,11 @@ package guru.springframework.msscbeerservice.config;
 
 import feign.auth.BasicAuthRequestInterceptor;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+@EnableFeignClients
 @Configuration
 public class FeignClientConfig {
 
